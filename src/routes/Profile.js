559972ccb92868ab49b290a9ad08c1e378cf8b19
Dabee-ee <movie2529@naver.com>
2022-08-19 00:30:38 +0900

    Create Authentication features

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-export default () => <div>Profile</div>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <div>Profile</div>
+      <button onClick={onLogOutClick}> Logout </button>
+    </>
+  );
+};
+
+export default Profile;
